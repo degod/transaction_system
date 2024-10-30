43a97af5bfd38a2cb5a8009b6ae27d8ae8be3e51
Godwin Uche <godwin.uche@paynovate.com>
2024-10-30 03:34:11 +0100

    Created migrations, factories and models required

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(), // Creates a user and assigns their ID
+            'type' => fake()->randomElement(['deposit', 'withdrawal']),
+            'amount' => fake()->randomFloat(2, 10, 10000), // Amount between 10 and 10,000
+            'balance_after' => fake()->randomFloat(2, 100, 50000), // Balance between 100 and 50,000
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -11,11 +10,6 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
     /**
      * Define the model's default state.
      *
@@ -25,20 +19,10 @@ class UserFactory extends Factory
     {
         return [
             'name'              => fake()->name(),
-            'email'             => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password'          => static::$password ??= Hash::make('password'),
-            'remember_token'    => Str::random(10),
+            'uuid'              => fake()->unique()->safeEmail(),
+            'balance'           => fake()->randomFloat(2, 10, 10000),
+            'created_at'        => now(),
+            'updated_at'        => now(),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
